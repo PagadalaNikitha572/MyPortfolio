16dcda783fcba7eda48903da12b09a7619536382
NIKITHA PAGADALA <22wh1a0572@bvrithyderabad.edu.in>
2024-12-02 13:29:44 +0530

    build issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from './components/Contact';
 import Education from './components/Education';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Projects from './components/projects';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 function App() {
   return (
